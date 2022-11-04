a01d2b1303d8965c1a6b5c26cb6e27f90aa19328
Malavika-G-Raj <95291694+Malavika-G-Raj@users.noreply.github.com>
2022-11-04 09:23:20 +0530

    Add files via upload

--- dmerge.py
+++ dmerge.py
@@ -1,5 +1,6 @@
 d1={}
 d2={}
+d3={}
 a=int(input("Enter no of items in dictionary\n"))
 b=int(input("Enter no of items in dictionary\n"))
 for i in range(a):
@@ -12,6 +13,11 @@ for j in range(b):
    value=input("Enter value")
    d2[key]=value
 print(d2)
-print("\n After Merge\n")
+"""print("\n After Merge\n")
 d1.update(d2)
-print(d1)
+print(d1)"""
+d3={**d1,**d2}
+for k,v in d3.items():
+    if k in d1 and k in d2:
+         d3[k]=[v,d1[k]]
+print(d3)
